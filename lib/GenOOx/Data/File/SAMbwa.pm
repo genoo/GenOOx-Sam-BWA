737fa2804748bc2321356aa0c019d4d90a4d2894
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2014-03-04 11:29:35 -0500

    Create module GenOOx::Data::File::SAMbwa for documentation

---
+++ lib/GenOOx/Data/File/SAMbwa.pm
@@ -0,0 +1,57 @@
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+GenOOx::Data::File::SAMbwa - Module to support reading SAM files generated from BWA aligner
+
+=head1 SYNOPSIS
+
+This module serves as a plugin for the GenOO framework. To use it just include it in your scripts and ask GenOO SAM parser to use it.
+
+    my $file_parser = GenOO::Data::File::SAM->new(
+        file          => 'file.sam',
+        records_class => 'GenOOx::Data::File::SAMstar::Record'
+    );
+
+    while (my $record = $file_parser->next_record) {
+        # $record is now an instance of GenOOx::Data::File::SAMstar::Record.
+        print $record->cigar."\n"; # name
+        print $record->flag."\n"; # flag
+        print $record->number_of_mappings."\n"; # new stuff not present by default
+    }
+
+
+=head1 DESCRIPTION
+
+The GenOO framework SAM parser avoids code that is unique to specific programs and makes no assumptions for the optional fields in the file. This module serves as a plugin for the GenOO framework and provides the functionality for reading SAM files generated from the BWA aligner. The module has been created on top of the generic GenOO SAM parser and to use it just include it in your scripts and ask GenOO SAM parser to use it.
+
+=head1 EXAMPLES
+
+    # Create a parser
+    my $file_parser = GenOO::Data::File::SAM->new(
+        file          => 'file.sam',
+        records_class => 'GenOOx::Data::File::SAMstar::Record'
+    );
+
+    # Loop on the records of the file
+    while (my $record = $file_parser->next_record) {
+        # $record is now an instance of GenOOx::Data::File::SAMstar::Record.
+        print $record->cigar."\n"; # name
+        print $record->flag."\n"; # flag
+        print $record->number_of_mappings."\n"; # new stuff not present by default in GenOO
+    }
+
+=cut
+
+# Let the code begin...
+
+package GenOOx::Data::File::SAMbwa;
+
+
+#######################################################################
+#######################   Load External modules   #####################
+#######################################################################
+use Modern::Perl;
+
+
+1;
